e786d5051d0adeb8234ccda09f0ed78c7fc3d7cf
ifad <yusuffadhlih123@gmail.com>
2025-12-01 22:38:30 +0700

    Mengubah efek hover dan mengganti dari rounded-lg ke rounded-full

--- resources/js/Components/SelectBox.jsx
+++ resources/js/Components/SelectBox.jsx
@@ -1,22 +1,14 @@
 export default function SelectBox({ icon: Icon, label, active, onClick }) {
-    // Kelas CSS yang dinamis berdasarkan prop 'active'
-    // Ganti 'border-quaternary' dan 'ring-quaternary' dengan kelas warna yang Anda definisikan di Tailwind
-    const quaternaryBorderClass = "border-quaternary ring-2 ring-quaternary";
-
     return (
         <div className="flex flex-col items-center">
             <button
-                // Panggil fungsi onClick yang diteruskan dari komponen induk
                 onClick={onClick}
                 className={`
-                    bg-white border rounded-xl md:p-[2.5rem] p-[2rem] gap-[1rem] hover:opacity-80 
-                    transition duration-200 ease-in-out
-                    
-                    ${active ? quaternaryBorderClass : "border-gray-200"} 
-                    ${active ? "fill-tertiary" : "fill-secondary"}
-                `}
+                 rounded-full md:p-[2.5rem] p-[2rem] gap-[1rem] hover:opacity-80 
+                transition duration-300 ease-in-out
+                ${active ? "text-white bg-tertiary" : "bg-white"}
+    `}
             >
-                {/* Icon adalah komponen React yang diteruskan sebagai prop */}
                 <Icon size={54} />
             </button>
 
